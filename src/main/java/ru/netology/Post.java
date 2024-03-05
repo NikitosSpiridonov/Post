570a919dd4fd9c106bbe763eb8c5eef2b80f6d7a
Nikita Spiridonov <spiridonov.n.i@yandex.ru>
2024-03-05 21:46:41 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport;
+
+    public String patronymic;
+
+    public String phone;
+
+    public String surname;
+    public FormDate birthday;
+
+    public boolean subscription;
+}
